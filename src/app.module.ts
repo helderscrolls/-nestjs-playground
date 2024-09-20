75947ab75f1001aa6ba95776a7a06e54e6adcd3c
helderscrolls <nortehelder@gmail.com>
2024-09-20 18:10:38 +0200

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { DatabaseModule } from './modules/database/database.module';
+import { PostsModule } from './modules/posts/posts.module';
+
+@Module({
+  imports: [
+    PostsModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+    DatabaseModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/modules/posts/posts.controller.ts
@@ -0,0 +1,41 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreatePostDto, UpdatePostDto } from './dto';
+import PostsService from './services/posts.service';
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  getAllPosts() {
+    return this.postsService.getAllPosts();
+  }
+
+  @Get(':id')
+  getPostById(@Param('id') id: string) {
+    return this.postsService.getPostById(Number(id));
+  }
+
+  @Post()
+  async createPost(@Body() post: CreatePostDto) {
+    return this.postsService.createPost(post);
+  }
+
+  @Put(':id')
+  async updatePost(@Param('id') id: string, @Body() post: UpdatePostDto) {
+    return this.postsService.updatePost(Number(id), post);
+  }
+
+  @Delete(':id')
+  async deletePost(@Param('id') id: string) {
+    return this.postsService.deletePost(Number(id));
+  }
+}
